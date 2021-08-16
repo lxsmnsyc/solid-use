8b0a5e4b8fbe3bd1f94c548a1cfcb15797ac61a5
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-08-16 23:36:14 +0800

    Add `equals` to `atom`

--- packages/solid-use/src/atom.ts
+++ packages/solid-use/src/atom.ts
@@ -1,12 +1,15 @@
 import { createSignal } from 'solid-js';
+import { is } from './utils';
 
 export interface Atom<T> {
   (): T;
   (value: T): T;
 }
 
-export default function atom<T>(value: T): Atom<T> {
-  const [state, setState] = createSignal(value);
+export default function atom<T>(value: T, equals = is): Atom<T> {
+  const [state, setState] = createSignal(value, {
+    equals,
+  });
   return (...args: [] | [T]): T => {
     if (args.length === 1) {
       setState(() => args[0]);
