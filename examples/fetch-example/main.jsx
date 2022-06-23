7854b04963477a36b46c3f775e0efddac71ade6f
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-06-23 20:42:29 +0800

    Add `fetch` example

---
+++ examples/fetch-example/main.jsx
@@ -0,0 +1,25 @@
+import { Suspense } from 'solid-js';
+import { render } from 'solid-js/web';
+import {
+  fetch,
+} from 'solid-use';
+
+function SuspensefulDogImage() {
+  const result = fetch('https://dog.ceo/api/breed/shiba/images/random', undefined, true).json();
+
+  return (
+    <Show when={result()}>
+      <img src={result().message} alt={result().message} />
+    </Show>
+  );
+}
+
+function App() {
+  return (
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <SuspensefulDogImage />
+    </Suspense>
+  );
+}
+
+render(() => <App />, document.getElementById('app'));
