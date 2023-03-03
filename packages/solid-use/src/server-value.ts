f1fc06cfdf5000720098a8665d4f1ebe46c62580
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-03-03 15:13:32 +0800

    Add `solid-use/server-value`

---
+++ packages/solid-use/src/server-value.ts
@@ -0,0 +1,86 @@
+import {
+  createComponent,
+  createContext,
+  createUniqueId,
+  JSX,
+  useContext,
+} from 'solid-js';
+import seroval, {
+  ServerValue,
+} from 'seroval';
+import {
+  isServer,
+  useAssets,
+} from 'solid-js/web';
+
+const ServerValueContext = createContext<Record<string, ServerValue>>();
+
+const SOLID_USE = '__SOLID_USE_SV__';
+
+function serializeServerValue<T extends ServerValue>(id: string, value: T): JSX.Element {
+  const target = `window.${SOLID_USE}`;
+  const init = `${target}=${target}||{}`;
+  const assignment = `(${init})[${JSON.stringify(id)}]=${seroval(value)};`;
+  return {
+    t: `<script>${assignment}</script>`,
+  } as unknown as JSX.Element;
+}
+
+declare const window: Window & {
+  [key in typeof SOLID_USE]: Record<string, ServerValue>;
+};
+
+export function useServerValue<T extends ServerValue>(source: () => T): T {
+  const id = createUniqueId();
+
+  const record = useContext(ServerValueContext);
+
+  if (record) {
+    if (isServer) {
+      record[id] = source();
+    }
+    return record[id] as T;
+  }
+  if (isServer) {
+    const value = source();
+    useAssets(() => serializeServerValue(id, value));
+    return value;
+  }
+  return window[SOLID_USE][id] as T;
+}
+
+export interface ServerValueBoundaryProps {
+  values: Record<string, ServerValue>;
+  children: JSX.Element;
+}
+
+function serializeServerValues(value: Record<string, ServerValue>): JSX.Element {
+  const target = `window.${SOLID_USE}`;
+  const assignment = `${target}=Object.assign(${target}||{},${seroval(value)});`;
+  return {
+    t: `<script>${assignment}</script>`,
+  } as unknown as JSX.Element;
+}
+
+export function useSerializeServerValues(
+  values: Record<string, ServerValue>,
+): void {
+  useAssets(() => serializeServerValues(values));
+}
+
+export function ServerValueBoundary(props: ServerValueBoundaryProps): JSX.Element {
+  const values = props.values || {};
+  if (!isServer) {
+    Object.assign(values, window[SOLID_USE]);
+  } else if (!props.values) {
+    useAssets(() => serializeServerValues(values));
+  }
+  return (
+    createComponent(ServerValueContext.Provider, {
+      value: values,
+      get children() {
+        return props.children;
+      },
+    })
+  );
+}
